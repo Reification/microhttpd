99e5bf904b3866737ad7a7654932476caae29868
Benn Herrera <bherrera@reification.io>
2018-05-08 10:57:13 -0700

    added default android toolchain file

---
+++ android.toolchain.cmake
@@ -0,0 +1,3 @@
+#include the toolchain file for generating visual studio projects using mobile c++ android workflow
+#see https://github.com/Reification/CMake/releases if you want to generate for MSVS android c++ workflow
+include("${CMAKE_ROOT}/msvs-android/android.toolchain.cmake")
